1f995f7e8178d552a082e9976c02fccd78d1c392
GustavoLemoos <gustavo.aulas@outlook.com>
2022-06-26 15:14:46 -0300

    PessoaServico em desenvolvimento.

--- Entra21.TrabalhoWindowsForm/PessoaServico.cs
+++ Entra21.TrabalhoWindowsForm/PessoaServico.cs
@@ -8,5 +8,35 @@ namespace Entra21.TrabalhoWindowsForm
 {
     internal class PessoaServico
     {
+        private string ArquivoLocal = "pessoa.json";
+        private List<Pessoa> pessoas;
+
+        public void 
+        public void Adicionar()
+        {
+
+        }
+
+        public void Apagar()
+        {
+
+        }
+
+        public void Editar()
+        {
+
+        }
+
+        public void LerArquivo()
+        {
+
+        }
+
+        public void SalvarArquivo()
+        {
+
+        }
+
+
     }
 }
