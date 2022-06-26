52026dcaafe5c9fae842f207a7f6b876af12ec4f
GustavoLemoos <gustavo.aulas@outlook.com>
2022-06-26 15:08:52 -0300

    Criação das classes e serviços referentes à Pessoa e Status. Classes e serviços a desenvolver.

---
+++ Entra21.TrabalhoWindowsForm/Status.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entra21.TrabalhoWindowsForm
+{
+    internal class Status
+    {
+        public int Codigo;
+        public DateTime DataCadastro;
+        public Pessoa Pessoa;
+        public string Profissao;
+        public string EmpresaTrabalho;
+        public double Salario;
+        public bool NomeLimpo;
+        public double LimiteFinanceiro;
+    }
+}
