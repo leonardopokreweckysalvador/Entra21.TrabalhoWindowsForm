00c250d62a1bfc7ffc929d3f5abb09dd328f29f0
willljdev <willljdev@gmail.com>
2022-06-26 18:34:28 -0300

    Criação dos métodos LerArquivo; Adicionar; Apagar e Editar.

--- Entra21.TrabalhoWindowsForm/CarroServico.cs
+++ Entra21.TrabalhoWindowsForm/CarroServico.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,68 @@ namespace Entra21.TrabalhoWindowsForm
 {
     internal class CarroServico
     {
+        private string arquivoLocal = "carro.Json";
+
+        private List<Carro> carros;
+
+        public CarroServico()
+        {
+            carros = new List<Carro>();
+
+            LerArquivo();
+        }
+
+        public void LerArquivo()
+        {
+            if (File.Exists(arquivoLocal) == false)
+                return;
+
+            var carroEmJson = File.ReadAllText(arquivoLocal);
+            carros = JsonConvert.DeserializeObject<List<Carro>>(carroEmJson);
+        }
+
+        public void Adicionar(Carro adicionarCarro)
+        {
+            carros.Add(adicionarCarro);
+
+            SalvarArquivo();
+        }
+
+        public void Apagar(Carro apagarCarro)
+        {
+            for (var i = 0; i < carros.Count; i++)
+            {
+                var carro = carros[i];
+                if(carro.Codigo == apagarCarro.Codigo)
+                {
+                    carros.Remove(apagarCarro);
+
+                    SalvarArquivo();
+                    return;
+                }
+            }
+        }
+        public void Editar(Carro editarCarro)
+        {
+            for (var i = 0; i < carros.Count; i++)
+            {
+                var carro = carros[i];
+
+                if (carro.Codigo == editarCarro.Codigo)
+                {
+                    carro.Proprietario = editarCarro.Proprietario;
+                    carro.Loja = editarCarro.Loja;
+                    carro.Categoria = editarCarro.Categoria;
+                    carro.Renavam = editarCarro.Renavam;
+                    carro.Placa = editarCarro.Placa;
+                    carro.AnoFabricacao = editarCarro.AnoFabricacao;
+                    carro.AnoModelo = editarCarro.AnoModelo;
+
+                    SalvarArquivo();
+                    return;
+                }
+            }
+        }
+
     }
 }
