450a7c06d6d23d44758e939e50beddf19d7aa18e
GustavoLemoos <gustavo.aulas@outlook.com>
2022-06-26 16:33:09 -0300

    PessoaServico e StatusServico em desenvolvimento.

--- Entra21.TrabalhoWindowsForm/StatusServico.cs
+++ Entra21.TrabalhoWindowsForm/StatusServico.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -11,29 +12,58 @@ namespace Entra21.TrabalhoWindowsForm
         private string ArquivoLocal = "status.json";
         private List<Status> statuses;
 
-        public void Adcionar()
+        public void Adcionar(Status encoder)
         {
-
+            statuses.Add(encoder);
+            SalvarArquivo();
         }
 
-        public void Apagar()
+        public void Apagar(Status statusApagar)
         {
-
+            for (var i = 0; i < statuses.Count; i++)
+            {
+                var status = statuses[i];
+                if (status.Codigo == statusApagar.Codigo)
+                {
+                    statuses.Remove(status);
+                    SalvarArquivo();
+                    return;
+                }
+            }
         }
 
-        public void Editar()
+        public void Editar(Status statusEditar)
         {
-
+            for (var i = 0; i < statuses.Count; i++)
+            {
+                var status = statuses[i];
+                if (status.Codigo == statusEditar.Codigo)
+                {
+                    status.DataCadastro = statusEditar.DataCadastro;
+                    status.Pessoa = statusEditar.Pessoa;
+                    status.Profissao = statusEditar.Profissao;
+                    status.EmpresaTrabalho = statusEditar.EmpresaTrabalho;
+                    status.Salario = statusEditar.Salario;
+                    status.NomeLimpo = statusEditar.NomeLimpo;
+                    status.LimiteFinanceiro = statusEditar.LimiteFinanceiro;
+                    SalvarArquivo();
+                    return;
+                }
+            }
         }
 
         public void LerArquivo()
         {
-
+            if (File.Exists(ArquivoLocal) == false)
+                return;
+            var statusesEmJson = File.ReadAllText(ArquivoLocal);
+            statuses = JsonConvert.DeserializeObject<List<Status>>(statusesEmJson);
         }
 
         public void SalvarArquivo()
         {
-
+            var statusesEmJson = JsonConvert.SerializeObject(statuses);
+            File.WriteAllText(ArquivoLocal, statusesEmJson);
         }
 
         public Status ObterPorCodigo(int codigo)
@@ -65,6 +95,5 @@ namespace Entra21.TrabalhoWindowsForm
 
             return ultimoCodigo;
         }
-
     }
 }
