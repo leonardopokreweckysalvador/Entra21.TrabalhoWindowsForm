aee7b32a331cd5517f3ec77c80930f506e3a9ec3
willljdev <willljdev@gmail.com>
2022-06-29 19:37:52 -0300

    aa

--- Entra21.TrabalhoWindowsForm/Carro.cs
+++ Entra21.TrabalhoWindowsForm/Carro.cs
@@ -11,6 +11,7 @@ namespace Entra21.TrabalhoWindowsForm
         public int Codigo;
         public Pessoa Proprietario;
         public Concessionaria Loja;
+
         public EnumCategoria Categoria;
         public string Renavam;
         public string Placa;
