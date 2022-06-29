9995a7ccf39df70d261e2da0388f578a53aec461
willljdev <willljdev@gmail.com>
2022-06-29 14:04:03 -0300

    Inserção dos botões Carros e Marcas, no Form MenuPrincipal

--- Entra21.TrabalhoWindowsForm/MenuPrincipal.Designer.cs
+++ Entra21.TrabalhoWindowsForm/MenuPrincipal.Designer.cs
@@ -31,13 +31,16 @@
             this.buttonEndereco = new System.Windows.Forms.Button();
             this.buttonpPessoa = new System.Windows.Forms.Button();
             this.buttonStatus = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.buttonCarros = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // buttonEndereco
             // 
-            this.buttonEndereco.Location = new System.Drawing.Point(23, 12);
+            this.buttonEndereco.Location = new System.Drawing.Point(26, 16);
+            this.buttonEndereco.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.buttonEndereco.Name = "buttonEndereco";
-            this.buttonEndereco.Size = new System.Drawing.Size(177, 100);
+            this.buttonEndereco.Size = new System.Drawing.Size(202, 133);
             this.buttonEndereco.TabIndex = 0;
             this.buttonEndereco.Text = "Endereço";
             this.buttonEndereco.UseVisualStyleBackColor = true;
@@ -45,9 +48,10 @@
             // 
             // buttonpPessoa
             // 
-            this.buttonpPessoa.Location = new System.Drawing.Point(232, 12);
+            this.buttonpPessoa.Location = new System.Drawing.Point(265, 16);
+            this.buttonpPessoa.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.buttonpPessoa.Name = "buttonpPessoa";
-            this.buttonpPessoa.Size = new System.Drawing.Size(177, 100);
+            this.buttonpPessoa.Size = new System.Drawing.Size(202, 133);
             this.buttonpPessoa.TabIndex = 1;
             this.buttonpPessoa.Text = "Pessoa";
             this.buttonpPessoa.UseVisualStyleBackColor = true;
@@ -55,22 +59,46 @@
             // 
             // buttonStatus
             // 
-            this.buttonStatus.Location = new System.Drawing.Point(232, 127);
+            this.buttonStatus.Location = new System.Drawing.Point(265, 169);
+            this.buttonStatus.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.buttonStatus.Name = "buttonStatus";
-            this.buttonStatus.Size = new System.Drawing.Size(177, 100);
+            this.buttonStatus.Size = new System.Drawing.Size(202, 133);
             this.buttonStatus.TabIndex = 2;
             this.buttonStatus.Text = "Status";
             this.buttonStatus.UseVisualStyleBackColor = true;
             this.buttonStatus.Click += new System.EventHandler(this.buttonStatus_Click);
             // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(502, 169);
+            this.button1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(202, 133);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Pessoa";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // buttonCarros
+            // 
+            this.buttonCarros.Location = new System.Drawing.Point(502, 16);
+            this.buttonCarros.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.buttonCarros.Name = "buttonCarros";
+            this.buttonCarros.Size = new System.Drawing.Size(202, 133);
+            this.buttonCarros.TabIndex = 4;
+            this.buttonCarros.Text = "Carros";
+            this.buttonCarros.UseVisualStyleBackColor = true;
+            // 
             // MenuPrincipal
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(652, 244);
+            this.ClientSize = new System.Drawing.Size(745, 325);
+            this.Controls.Add(this.buttonCarros);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.buttonStatus);
             this.Controls.Add(this.buttonpPessoa);
             this.Controls.Add(this.buttonEndereco);
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.Name = "MenuPrincipal";
             this.Text = "MenuPrincipal";
             this.ResumeLayout(false);
@@ -82,5 +110,7 @@
         private Button buttonEndereco;
         private Button buttonpPessoa;
         private Button buttonStatus;
+        private Button button1;
+        private Button buttonCarros;
     }
 }
