56e664f76f1d7013a0393c5bbc2795652c504c37
willljdev <willljdev@gmail.com>
2022-06-29 22:02:42 -0300

    UpDate CarroForm.cs

--- Entra21.TrabalhoWindowsForm/CarroForm.Designer.cs
+++ Entra21.TrabalhoWindowsForm/CarroForm.Designer.cs
@@ -586,6 +586,7 @@
             this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "CarroForm";
             this.Text = "CarroForm";
+            this.Load += new System.EventHandler(this.CarroForm_Load);
             ((System.ComponentModel.ISupportInitialize)(this.dataGridViewCarroForm)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
